7f7613585aa92b56f54b93253285c77041014bea
Maria Navarro <mariadvnr04@gmail.com>
2025-06-04 16:06:00 -0400

    LITO

--- src/views/HomePage.jsx
+++ src/views/HomePage.jsx
@@ -32,7 +32,7 @@ const HomePage = () => {
     const navigate = useNavigate()
 
     const handleLoginClick = () => {
-        navigate('/mainPage');
+        navigate('/login');
     }
 
     return(
