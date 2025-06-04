ef8ec354147477b3dc632ca5b9e02ac87cc0d243
Maria Navarro <mariadvnr04@gmail.com>
2025-06-04 08:39:36 -0400

    pruebra

--- src/views/HomePage.jsx
+++ src/views/HomePage.jsx
@@ -32,7 +32,7 @@ const HomePage = () => {
     const navigate = useNavigate()
 
     const handleLoginClick = () => {
-        navigate('/login');
+        navigate('/Login');
     }
 
     return(
---
+++ src/views/MainPage.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import "./styles/MainPage-style.css"
+
+const MainPage = () => {
+    return(
+        <div className="container-Main">
+            <h1>Music.fy</h1>
+        </div> 
+    )
+}
+
+export default MainPage
