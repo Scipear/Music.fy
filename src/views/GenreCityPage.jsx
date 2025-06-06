a2ce3992768d104e08062406d676fafac31a1b45
Scipear <Pantauwu09@gmail.com>
2025-06-06 00:37:39 -0400

    Arreglado to chico

---
+++ src/views/GenreCityPage.jsx
@@ -0,0 +1,58 @@
+import React, { useEffect, useState } from "react"
+import { useLocation } from "react-router-dom"
+import "./styles/ListenedPage-style.css"
+import ListenedSongs from "../components/ListenedSongs"
+import Sidebar from "../components/Sidebar"
+
+const GenreCityPage = () => {
+    const location = useLocation();
+    const userid = location.state?.usuario || null;
+    const nombre = location.state?.nameUser || null;
+
+    /*const [songsByDate, setSongsByDate] = useState({});
+
+    useEffect(() => {
+        const getSongs = async () => {
+            try{
+                const response = await fetch(`http://localhost:3000/usersongs/${userid}`);
+                const data = await response.json()
+                console.log("Datos desde backend:", data.songs[2]);
+                const agrupadas = agruparPorFecha(data.songs); // Asegúrate de que el back devuelva `songs`
+                console.log("Agrupadas por fecha:", agrupadas);
+                setSongsByDate(agrupadas);
+            }catch(error){
+                setMensaje("error desde el front");
+                console.error("Error al obtener las canciones", error);
+            }
+        };
+
+        getSongs();
+    }, []);*/
+    
+
+    return(
+        <div className="listened-container">
+            <Sidebar 
+                user={nombre}
+                userid={userid}
+                opt1={"Volver"} 
+                opt1link={"/mainPage"} 
+                opt2={"Canciones mas famosas por genero y ciudad"} 
+                opt2link={"/recently-listened"} 
+                opt3={"Canciones mas famosas por mes"} 
+                opt3link={"/recently-listened"}/>
+            <div className="header-container">
+                <h1>Music.fy</h1>
+            </div>
+
+            <div className="listened-main-container">
+                <h1>ESCUCHADO RECIENTEMENTE</h1>
+                {Object.entries(songsByDate).map(([fecha, canciones]) => (
+                    <ListenedSongs key={fecha} date={fecha} songs={canciones} />
+                ))}
+            </div>
+        </div>
+    )
+}
+
+export default GenreCityPage
--- src/views/MainPage.jsx
+++ src/views/MainPage.jsx
@@ -1,3 +1,5 @@
+import React from "react";
+import { useLocation } from "react-router-dom"
 import Sidebar from "../components/Sidebar";
 import "./styles/MainPage-style.css"
 
