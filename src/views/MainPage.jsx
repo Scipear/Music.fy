deef85e564954ba1644cce1080606bc23b6ece25
Scipear <Pantauwu09@gmail.com>
2025-06-05 23:05:38 -0400

    Barra lateral fixeada

--- src/views/MainPage.jsx
+++ src/views/MainPage.jsx
@@ -1,51 +1,32 @@
 import React from "react";
+import { useLocation } from "react-router-dom"
 import Sidebar from "../components/Sidebar";
 import "./styles/MainPage-style.css"
-import "./styles/Sidebar-style.css"// y esta es 
 
 const MainPage = () => {
+    const location = useLocation();
+    const userid = location.state?.usuario || null;
+
     return(
-       
         <div className="MainPage-container">
-           
-            <div className="sidebar">
-               
-            <div className="menu">
-                    <label htmlFor="btn-menu"><i for="btn-menu" class="fa-solid fa-bars"></i></label>
-                 </div>
-          <div className="check">
-            <input type="checkbox" id="btn-menu" />
-          </div>
-           
-              
-            <div className="sidebar-container">
-
-                    <Sidebar date='Arturo Padilla'/> 
-                        <ul>
-                            <li>
-                                <a href="">Escuchado recientemente</a>
-                            </li>
-
-                            <li>
-                                <a href="">Cerra Sesion</a>
-                            </li>
-                        </ul>
-            <label htmlFor=""><i class="fa-solid fa-xmark"></i></label>
-            </div>
-            
-            </div>     
-           
+            <Sidebar 
+                user={"Arturo Padilla"}
+                userid={userid}
+                opt1={"Escuchados recientemente"} 
+                opt1link={"/recently-listened"} 
+                opt2={"Canciones mas famosas por genero y ciudad"} 
+                opt2link={"/recently-listened"} 
+                opt3={"Canciones mas famosas por mes"} 
+                opt3link={"/recently-listened"}/>
 
             <div className="header-container">
                 <h1>Music.fy</h1>
             </div> 
-
-
             <div className="buscador">
-                    <input type="text" placeholder ="Buscar artista..."/>
-                        <div className="Busbutton-container">
-                            <button> <i class="fa-duotone fa-solid fa-magnifying-glass"></i></button>    
-                        </div>     
+                <input type="text" placeholder ="Buscar artista..."/>
+                <div className="Busbutton-container">
+                    <button> <i class="fa-duotone fa-solid fa-magnifying-glass"></i></button>    
+                </div>     
             </div>
 
              <div className = "rec-container">
@@ -53,12 +34,7 @@ const MainPage = () => {
                     <h1>RECOMENDADO PARA TI</h1>
                 </div>
             </div>
-
-
-
         </div>
-           
-        
     )
 }
 
