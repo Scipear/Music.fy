a1ce34dcd18fce61bb726931a7089e9673a3a6a9
Maria Navarro <mariadvnr04@gmail.com>
2025-06-05 23:47:51 -0400

    lolo

--- src/views/MainPage.jsx
+++ src/views/MainPage.jsx
@@ -1,51 +1,30 @@
-import React from "react";
 import Sidebar from "../components/Sidebar";
 import "./styles/MainPage-style.css"
-import "./styles/Sidebar-style.css"
 
 const MainPage = () => {
+    const location = useLocation();
+    const userid = location.state?.usuario || null;
+
     return(
-       
         <div className="MainPage-container">
-           
-            <div className="sidebar">
-                <div className="sidebar-container">
-                    <Sidebar date='Arturo Padilla'/> 
-                        <ul>
-                            <li>
-                                <a href="">Escuchado recientemente</a>
-                            </li>
-
-                            <li>
-                                <a href="">Cerra Sesion</a>
-                            </li>
-                        </ul>
-                <label htmlFor=""><i class="fa-solid fa-xmark"></i></label>
-                </div>
-           
-            </div>    
-
-
-            <div className="icon-container">
-               <label htmlFor="btn-menu"><i for="btn-menu" class="fa-solid fa-bars"></i></label>
-            </div>
-
-            <div className="cheack-container">
-                <input type="checkbox" id="btn-menu" />
-            </div>
-
+            <Sidebar 
+                user={"Arturo Padilla"}
+                userid={userid}
+                opt1={"Escuchados recientemente"} 
+                opt1link={"/recently-listened"} 
+                opt2={"Canciones mas famosas por genero y ciudad"} 
+                opt2link={"/recently-listened"} 
+                opt3={"Canciones mas famosas por mes"} 
+                opt3link={"/recently-listened"}/>
 
             <div className="header-container">
                 <h1>Music.fy</h1>
-
             </div> 
-
-
             <div className="buscador">
-                    <input type="text" placeholder ="Buscar artista..."/>
-                        <div className="Busbutton-container">
-                            <button> <i class="fa-duotone fa-solid fa-magnifying-glass"></i></button>    
-                        </div>     
+                <input type="text" placeholder ="Buscar artista..."/>
+                <div className="Busbutton-container">
+                    <button> <i class="fa-duotone fa-solid fa-magnifying-glass"></i></button>    
+                </div>     
             </div>
 
              <div className = "rec-container">
@@ -53,14 +32,8 @@ const MainPage = () => {
                     <h1>RECOMENDADO PARA TI</h1>
                 </div>
             </div>
-
-
-
         </div>
-           
-        
     )
 }
 
 export default MainPage
-
